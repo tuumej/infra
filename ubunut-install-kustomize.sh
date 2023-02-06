cb5ba36dac45389666d63c42794f57798c482e6d
tuumej <46086015+tuumej@users.noreply.github.com>
2023-02-06 18:17:39 +0900

    Create ubunut-install-kustomize.sh

---
+++ ubunut-install-kustomize.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+## INFO: https://kubectl.docs.kubernetes.io/installation/kustomize/binaries/
+
+set -euf -o pipefail
+
+KUSTOMIZE_VERSION=v4.4.1
+
+# Download kustomize binary
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+
+# Install to /usr/local/bin
+sudo install -o root -m 0755 kustomize /usr/local/bin/kustomize
